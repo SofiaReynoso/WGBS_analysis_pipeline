69c77ab1866607d04da03390ee88bcf4e48f1975
SofiaReynoso <102271775+SofiaReynoso@users.noreply.github.com>
2025-12-17 14:26:50 -0300

    Delete wgbs_scripts/create_cutadapt_env.sh

--- wgbs_scripts/create_cutadapt_env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-module load python-miniconda3/4.12.0
-
-
-conda create -n cutadapt_env -c bioconda cutadapt -y
-conda activate cutadapt_env
-
-conda install -c bioconda trim-galore -y
