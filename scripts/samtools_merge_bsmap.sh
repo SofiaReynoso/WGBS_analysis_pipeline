140d8759146ad8edb7bca2fb1230f411a9f6567c
SofiaReynoso <102271775+SofiaReynoso@users.noreply.github.com>
2025-12-17 15:10:06 -0300

    Update samtools_merge_bsmap.sh

--- scripts/samtools_merge_bsmap.sh
+++ scripts/samtools_merge_bsmap.sh
@@ -5,16 +5,16 @@ source /opt/conda/etc/profile.d/conda.sh
 conda activate bsmap_env
 
 INPUT_DIR=/work/data/bsmap
-OUTPUT_DIR=/work/data/merged
+OUTPUT_DIR=/work/data/bsmap/merged
 THREADS=4
 
 mkdir -p "$OUTPUT_DIR"
 
-OUTPUT_FILE="$OUTPUT_DIR/merged.bam"
+OUTPUT_FILE="$OUTPUT_DIR/merged.bam" #modificar segun interes
 
 samtools merge -@ "$THREADS" "$OUTPUT_FILE" \
-  "$INPUT_DIR/ENCFF001ZVP_mapped.bam" \
-  "$INPUT_DIR/ENCFF001ZVM_mapped.bam"
+  "$INPUT_DIR/ENCFF001ZVP_mapped.bam" \ #modificar segun interes
+  "$INPUT_DIR/ENCFF001ZVM_mapped.bam" #modificar segun interes
 
 conda deactivate
 
